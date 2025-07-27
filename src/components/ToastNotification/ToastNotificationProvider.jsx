a56813d2754b10ff63bf5813ae403cafe7e2daf2
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-07-27 22:31:01 +0200

    feat: - Fixed default state of ToastNotification to be set from enum.

--- src/components/ToastNotification/ToastNotificationProvider.jsx
+++ src/components/ToastNotification/ToastNotificationProvider.jsx
@@ -1,12 +1,17 @@
 import { useState, useCallback, useRef } from "react";
 import { ToastNotificationContext } from "./ToastNotificationContext";
 import ToastNotificationsContainer from "./ToastNotificationsContainer";
+import { toastNotificationTypesENUM } from "@enums";
 
 export default function ToastNotificationProvider({ children }) {
     const [toasts, setToasts] = useState([]);
     const idCounter = useRef(0);
 
-    const addToast = useCallback((message = "default message", type = "info", duration = 5) => {
+    const addToast = useCallback((
+        message = "default message", 
+        type = toastNotificationTypesENUM.INFO, 
+        duration = 5
+    ) => {
         const id = `toast-${idCounter.current++}`;
         setToasts((prev) => [{ id, message, type, duration }, ...prev]);
 
