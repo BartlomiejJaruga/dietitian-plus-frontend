f5f74cfea2bac95c1f20c3797a0f2dee1a55ceea
BartlomiejJaruga <245827@edu.p.lodz.pl>
2025-09-08 02:15:43 +0200

    feat: - Added removing and adding disliked products and allergies in patient "about me" tab

--- src/pages/PatientAboutMePage/PatientAboutMePage.jsx
+++ src/pages/PatientAboutMePage/PatientAboutMePage.jsx
@@ -7,13 +7,17 @@ import LoadingIndicator from "@components/LoadingIndicator/LoadingIndicator";
 import { useSelector } from "react-redux";
 import { useToastNotification } from "@hooks/useToastNotification";
 import { toastNotificationTypesENUM } from "@enums";
+import ProductSearchBar from "@components/ProductSearchBar/ProductSearchBar";
+import ConfirmationModal from "@components/ConfirmationModal/ConfirmationModal";
 
 export default function PatientAboutMePage() {
     const toastNotification = useToastNotification();
     const patientId = useSelector((state) => state.auth.userData.uuid);
+
     const [loadedPatientData, setLoadedPatientData] = useState({});
     const [loadedPatientAllergies, setLoadedPatientAllergies] = useState([]);
     const [loadedPatientDislikedProducts, setLoadedPatientDislikedProducts] = useState([]);
+    const [loadedProducts, setLoadedProducts] = useState([]);
     const [isPageBeingLoaded, setIsPageBeingLoaded] = useState(false);
 
     const [editableData, setEditableData] = useState({
@@ -23,6 +27,16 @@ export default function PatientAboutMePage() {
     });
     const [isEdited, setIsEdited] = useState(false);
 
+    const [isAllergyModalOpen, setIsAllergyModalOpen] = useState(false);
+    const [selectedProductId, setSelectedProductId] = useState(null);
+    const [productToRemove, setProductToRemove] = useState(null);
+    const [isRemoveModalOpen, setIsRemoveModalOpen] = useState(false);
+
+    const [isDislikedModalOpen, setIsDislikedModalOpen] = useState(false);
+    const [selectedDislikedProductId, setSelectedDislikedProductId] = useState(null);
+    const [productToRemoveDisliked, setProductToRemoveDisliked] = useState(null);
+    const [isRemoveDislikedModalOpen, setIsRemoveDislikedModalOpen] = useState(false);
+
     useEffect(() => {
         const loadInitialPatient = async () => {
             setIsPageBeingLoaded(true);
@@ -30,7 +44,8 @@ export default function PatientAboutMePage() {
             await Promise.all([
                 loadPatientData(),
                 loadPatientAllergies(),
-                loadPatientDislikedProducts()
+                loadPatientDislikedProducts(),
+                loadProducts()
             ]);
 
             setIsPageBeingLoaded(false);
@@ -66,6 +81,15 @@ export default function PatientAboutMePage() {
         try {
             const response = await axiosInstance.get(`/v1/patients/${patientId}/disliked-products`);
             setLoadedPatientDislikedProducts(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const loadProducts = async () => {
+        try {
+            const response = await axiosInstance.get("/v1/products");
+            setLoadedProducts(response.data);
         } catch (error) {
             console.error(error);
         }
@@ -128,6 +152,153 @@ export default function PatientAboutMePage() {
                 "Failed to update your information. Please try again later.",
                 toastNotificationTypesENUM.ERROR
             );
+        }
+    };
+
+    const handleConfirmAllergy = async () => {
+        if (!selectedProductId) return;
+        
+        try {
+            const requestBody = {
+                product_id: selectedProductId,
+            };
+
+            await axiosInstance.post(
+                `/v1/patients/${patientId}/allergenic-products`, 
+                requestBody
+            );
+            
+            await loadPatientAllergies();
+
+            toastNotification(
+                "Successfully added new allergy.",
+                toastNotificationTypesENUM.SUCCESS
+            );
+        } catch (error) {
+            if(error.response?.status === 400){
+                toastNotification(
+                    "Allergic product is already assigned to patient.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+            else{
+                console.error(error);
+                toastNotification(
+                    "Failed to add new allergy. Please try again later.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+        } finally {
+            setIsAllergyModalOpen(false);
+            setSelectedProductId(null);
+        }
+    };
+
+    const handleCancelAllergy = () => {
+        setIsAllergyModalOpen(false);
+        setSelectedProductId(null);
+    };
+
+    const handleConfirmRemoveAllergy = async () => {
+        try {
+            await axiosInstance.delete(
+                `/v1/patients/${patientId}/allergenic-products/${productToRemove?.product_id}`
+            );
+            
+            await loadPatientAllergies();
+
+            toastNotification(
+                "Successfully removed allergy.",
+                toastNotificationTypesENUM.SUCCESS
+            );
+        } catch (error) {
+            if(error.response?.status === 400){
+                toastNotification(
+                    "Allergic product is not assigned to patient.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+            else{
+                console.error(error);
+                toastNotification(
+                    "Failed to remove allergy. Please try again later.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+        } finally {
+            setIsRemoveModalOpen(false);
+            setProductToRemove(null);
+        }
+    }
+
+    const handleConfirmDisliked = async () => {
+        if (!selectedDislikedProductId) return;
+        
+        try {
+            const requestBody = {
+                product_id: selectedDislikedProductId,
+            };
+
+            await axiosInstance.post(
+                `/v1/patients/${patientId}/disliked-products`, 
+                requestBody
+            );
+            
+            await loadPatientDislikedProducts();
+
+            toastNotification(
+                "Successfully added disliked product.",
+                toastNotificationTypesENUM.SUCCESS
+            );
+        } catch (error) {
+            if(error.response?.status === 400){
+                toastNotification(
+                    "Product is already in disliked list.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+            else{
+                console.error(error);
+                toastNotification(
+                    "Failed to add disliked product. Please try again later.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+        } finally {
+            setIsDislikedModalOpen(false);
+            setSelectedDislikedProductId(null);
+        }
+    };
+
+    const handleConfirmRemoveDisliked = async () => {
+        try {
+            await axiosInstance.delete(
+                `/v1/patients/${patientId}/disliked-products/${productToRemoveDisliked?.product_id}`
+            );
+            
+            await loadPatientDislikedProducts();
+
+            toastNotification(
+                "Successfully removed disliked product.",
+                toastNotificationTypesENUM.SUCCESS
+            );
+        } catch (error) {
+            if(error.response?.status === 400){
+                toastNotification(
+                    "Disliked product is not assigned to patient.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+            else{
+                console.error(error);
+                toastNotification(
+                    "Failed to remove disliked product. Please try again later.",
+                    toastNotificationTypesENUM.ERROR
+                );
+            }
+        } finally {
+            setIsRemoveDislikedModalOpen(false);
+            setProductToRemoveDisliked(null);
         }
     };
 
@@ -217,7 +388,15 @@ export default function PatientAboutMePage() {
 
                         <section className={styles.patient_allergies_and_disliked_products_section}>
                             <div className={styles.allergies_container}>
-                                <h3>Allergies</h3>
+                                <div className={styles.section_header_row}>
+                                    <h3>Allergies</h3>
+                                    <button 
+                                        className={styles.add_button}
+                                        onClick={() => setIsAllergyModalOpen(true)}
+                                    >
+                                        Add new allergy
+                                    </button>
+                                </div>
                                 <div className={styles.allergies_list}>
                                     <div className={styles.scrollbar_container}>
                                         {loadedPatientAllergies.length <= 0 && (
@@ -227,19 +406,36 @@ export default function PatientAboutMePage() {
                                             </div>
                                         )}
                                         {loadedPatientAllergies.length > 0 && loadedPatientAllergies.map((product) => (
-                                            <span 
-                                                key={product.product_id}
+                                            <div 
+                                                key={product.product_id} 
                                                 className={styles.list_item}
                                             >
-                                                {product.product_name}
-                                            </span>
+                                                <span>{product.product_name}</span>
+                                                <button
+                                                    className={styles.list_item_remove_button}
+                                                    onClick={() => {
+                                                        setProductToRemove(product);
+                                                        setIsRemoveModalOpen(true);
+                                                    }}
+                                                >
+                                                    &minus;
+                                                </button>
+                                            </div>
                                         ))}
                                     </div>
                                 </div>
                             </div>
 
                             <div className={styles.disliked_products_container}>
-                                <h3>Disliked products</h3>
+                                <div className={styles.section_header_row}>
+                                    <h3>Disliked products</h3>
+                                    <button 
+                                        className={styles.add_button}
+                                        onClick={() => setIsDislikedModalOpen(true)}
+                                    >
+                                        Add new product
+                                    </button>
+                                </div>
                                 <div className={styles.disliked_products_list}>
                                     <div className={styles.scrollbar_container}>
                                         {loadedPatientDislikedProducts.length <= 0 && (
@@ -249,12 +445,21 @@ export default function PatientAboutMePage() {
                                             </div>
                                         )}
                                         {loadedPatientDislikedProducts.length > 0 && loadedPatientDislikedProducts.map((product) => (
-                                            <span 
-                                                key={product.product_id}
+                                            <div 
+                                                key={product.product_id} 
                                                 className={styles.list_item}
                                             >
-                                                {product.product_name}
-                                            </span>
+                                                <span>{product.product_name}</span>
+                                                <button
+                                                    className={styles.list_item_remove_button}
+                                                    onClick={() => {
+                                                        setProductToRemoveDisliked(product);
+                                                        setIsRemoveDislikedModalOpen(true);
+                                                    }}
+                                                >
+                                                    &minus;
+                                                </button>
+                                            </div>
                                         ))}
                                     </div>
                                 </div>
@@ -263,6 +468,91 @@ export default function PatientAboutMePage() {
                     </div>
                 )}
             </div>
+
+            {/* Modal dodawania alergii */}
+            {isAllergyModalOpen && (
+                <div className={styles.modal_backdrop}>
+                    <div className={styles.modal}>
+                        <h2>Add new allergy</h2>
+                        <ProductSearchBar
+                            productsData={loadedProducts}
+                            onProductSelect={(id) => setSelectedProductId(id)}
+                            fontSize="1em"
+                        />
+                        <div className={styles.modal_actions}>
+                            <button 
+                                className={styles.button_confirm}
+                                onClick={handleConfirmAllergy}
+                                disabled={!selectedProductId}
+                            >
+                                Confirm
+                            </button>
+                            <button 
+                                className={styles.button_cancel}
+                                onClick={handleCancelAllergy}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Modal dodawania Disliked Product */}
+            {isDislikedModalOpen && (
+                <div className={styles.modal_backdrop}>
+                    <div className={styles.modal}>
+                        <h2>Add new disliked product</h2>
+                        <ProductSearchBar
+                            productsData={loadedProducts}
+                            onProductSelect={(id) => setSelectedDislikedProductId(id)}
+                            fontSize="1em"
+                        />
+                        <div className={styles.modal_actions}>
+                            <button 
+                                className={styles.button_confirm}
+                                onClick={handleConfirmDisliked}
+                                disabled={!selectedDislikedProductId}
+                            >
+                                Confirm
+                            </button>
+                            <button 
+                                className={styles.button_cancel}
+                                onClick={() => {
+                                    setIsDislikedModalOpen(false);
+                                    setSelectedDislikedProductId(null);
+                                }}
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {/* Modal potwierdzenia usunięcia alergii */}
+            <ConfirmationModal
+                isOpen={isRemoveModalOpen}
+                message={`Are you sure you want to remove allergy to ${productToRemove?.product_name}?`}
+                messageToHighlight={productToRemove?.product_name}
+                onConfirm={handleConfirmRemoveAllergy}
+                onCancel={() => {
+                    setIsRemoveModalOpen(false);
+                    setProductToRemove(null);
+                }}
+            />
+
+            {/* Modal potwierdzenia usunięcia Disliked Product */}
+            <ConfirmationModal
+                isOpen={isRemoveDislikedModalOpen}
+                message={`Are you sure you want to remove disliked product ${productToRemoveDisliked?.product_name}?`}
+                messageToHighlight={productToRemoveDisliked?.product_name}
+                onConfirm={handleConfirmRemoveDisliked}
+                onCancel={() => {
+                    setIsRemoveDislikedModalOpen(false);
+                    setProductToRemoveDisliked(null);
+                }}
+            />
         </>
     );
 }
